51740ebb28b488d411df67e9b41a02c465480cd0
VRAU NELAS DO 777 <151977214+Augustho08@users.noreply.github.com>
2025-04-19 20:07:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
