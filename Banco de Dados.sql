03cba3e4e15c7b13247806aaed700da5fa13cd68
VRAU NELAS DO 777 <151977214+Augustho08@users.noreply.github.com>
2025-05-05 11:58:07 -0300

    Update Banco de Dados.sql

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -1,12 +1,1 @@
-USE cadastro2;
-create TABLE usuarios (id INT auto_increment PRIMARY KEY,
-
-
-nome VARCHAR(100),
-email VARCHAR(100)UNIQUE,
-senha VARCHAR(100)
-);
-
-USE cadastro2;
-SELECT * FROM usuarios;
 
